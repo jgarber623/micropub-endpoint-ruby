b8beaad44d4d7d279e2d88b5fd67813a7add79e9
Jason Garber <jason@sixtwothree.org>
2018-06-30 16:14:28 -0400

    Add more specs

--- spec/lib/micropub/endpoint/discover_spec.rb
+++ spec/lib/micropub/endpoint/discover_spec.rb
@@ -12,4 +12,30 @@ describe Micropub::Endpoint::Discover do
       expect { described_class.new('http:') }.to raise_error(Micropub::Endpoint::InvalidURIError)
     end
   end
+
+  context 'when given a valid URL' do
+    let(:url) { 'https://example.com' }
+
+    before :each do
+      stub_request(:get, url)
+    end
+
+    describe '#response' do
+      it 'returns an HTTP::Response' do
+        expect(described_class.new(url).response).to be_an_instance_of(HTTP::Response)
+      end
+    end
+
+    describe '#uri' do
+      it 'returns an Addressable::URI' do
+        expect(described_class.new(url).uri).to be_an_instance_of(Addressable::URI)
+      end
+    end
+
+    describe '#url' do
+      it 'returns the URL' do
+        expect(described_class.new(url).url).to eq(url)
+      end
+    end
+  end
 end
