d598f1b7c40455aaf5ed8f9ad450e5439f65584f
Jason Garber <jason@sixtwothree.org>
2018-07-29 00:04:58 -0400

    Raise an ArgumentError when Client instantiated with non-absolute URI

--- spec/lib/micropub/endpoint/client_spec.rb
+++ spec/lib/micropub/endpoint/client_spec.rb
@@ -12,4 +12,12 @@ describe Micropub::Endpoint::Client do
       expect { described_class.new('http:') }.to raise_error(Micropub::Endpoint::InvalidURIError)
     end
   end
+
+  context 'when given a relative URL' do
+    let(:message) { 'url must be an absolute URI (e.g. https://example.com)' }
+
+    it 'raises an ArgumentError' do
+      expect { described_class.new('../foo/bar/biz/baz') }.to raise_error(Micropub::Endpoint::ArgumentError, message)
+    end
+  end
 end
